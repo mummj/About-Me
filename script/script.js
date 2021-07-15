45dfb2fa96555a90eb8d9a7417a53c684a3e2837
Justin Hamerly <suvimountain@icloud.com>
2021-07-15 13:29:48 -0700

    added question functions

--- script/script.js
+++ script/script.js
@@ -7,91 +7,69 @@ let name = prompt('Hello, Welcome to my site. Please tell me your name');
 alert('Welcome to my site ' + name + '. You are going to play a little guessing game about me.' );
 
 alert('please answer each of the following with yes or no answers.')
+
+function correct(){
+  if (teacher === 'yes' || teacher === 'y'){
+    // console.log('that is correct')
+    alert('That is correct!!')
+    score++;
+  } 
+  else if(teacher === 'no' || teacher === 'n'){
+  //  console.log('that is not correct')
+    alert('That is not correct!')
+  }
+  else{
+    alert('I did\'t understant that please try again')
+  }
+}
+function incorrect(){
+
+  
+  if (subject === 'yes' || subject === 'y'){
+    //  console.log('that is not correct')
+    alert('That is not correct!!')
+  } 
+  else if(subject === 'no' || subject === 'n'){
+    // console.log('that is correct')
+    alert('That is correct!')
+    score++;
+  }
+  else{
+    alert('I did\'t understant that please try again')
+  }
+}
 //Fisrt Question
 let teacherLow = prompt('Have I ever been a teacher?')
 
 let teacher = teacherLow.toLowerCase();
 
-if (teacher === 'yes' || teacher === 'y'){
-  // console.log('that is correct')
-  alert('That is correct!!')
-  score++;
-} 
-else if(teacher === 'no' || teacher === 'n'){
-//  console.log('that is not correct')
-  alert('That is not correct!')
-}
-else{
-  alert('I did\'t understant that please try again')
-}
+correct();
 //Second Question
 let subjectLow = prompt('Do I teach English?')
 
 let subject = subjectLow.toLowerCase();
 
-if (subject === 'yes' || subject === 'y'){
-  //  console.log('that is not correct')
-  alert('That is not correct!!')
-} 
-else if(subject === 'no' || subject === 'n'){
-  // console.log('that is correct')
-  alert('That is correct!')
-  score++;
-}
-else{
-  alert('I did\'t understant that please try again')
-}
+incorrect();
 //Thrid Question
 let coachLow = prompt('Do I coach a sport?')
 
 let coach = coachLow.toLowerCase();
 
-if (coach === 'yes' || coach === 'y'){
-  // console.log('that is correct')
-  alert('That is correct!!')
-  score++;
-} 
-else if(coach === 'no' || coach === 'n'){
-  //  console.log('that is not correct')
-  alert('That is not correct!')
-}
-else{
-  alert('I did\'t understant that please try again')
-}
+correct();
+
 //Fourth Question
 let levelLow = prompt('Do I teach at the Middle School')
 
 let level = levelLow.toLowerCase();
 
-if (level === 'yes' || level === 'y'){
-  //  console.log('that is not correct')
-  alert('That is not correct!!')
-} 
-else if(level === 'no' || level === 'n'){
-  // console.log('that is correct')
-  alert('That is correct!')
-  score++;
-}
-else{
-  alert('I did\'t understant that please try again')
-}
+incorrect();
+
 //Fifth Question
 let dirationLow = prompt('Have I been teaching for 7 years?')
 
 let diration = dirationLow.toLowerCase();
 
-if (diration === 'yes' || diration === 'y'){
-  // console.log('that is correct')
-  alert('That is correct!!')
-  score++;
-} 
-else if(diration === 'no' || diration === 'n'){
-  //  console.log('that is not correct')
-  alert('That is not correct!')
-}
-else{
-  alert('I did\'t understant that please try again')
-}
+correct();
 
 //Guess a number question.
 let myNumb = 100;
