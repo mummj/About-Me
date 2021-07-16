623039a89c793e741d5fa1ebdb381081630dfaf8
mummj <mummj@eou.edu>
2021-07-16 13:12:58 -0700

    final

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 'use strict'
 
-// console.log('hello world');
-//Introduction Question
+
 let score = 0
 let name = prompt('Hello, Welcome to my site. Please tell me your name');
 alert('Welcome to my site ' + name + '. You are going to play a little guessing game about me.' );
@@ -10,12 +9,10 @@ alert('please answer each of the following with yes or no answers.')
 
 function correct(){
   if (teacher === 'yes' || teacher === 'y'){
-    // console.log('that is correct')
     alert('That is correct!!')
     score++;
   } 
   else if(teacher === 'no' || teacher === 'n'){
-  //  console.log('that is not correct')
     alert('That is not correct!')
   }
   else{
@@ -24,11 +21,9 @@ function correct(){
 }
 function incorrect(){
   if (subject === 'yes' || subject === 'y'){
-    //  console.log('that is not correct')
     alert('That is not correct!!')
   } 
   else if(subject === 'no' || subject === 'n'){
-    // console.log('that is correct')
     alert('That is correct!')
     score++;
   }
@@ -37,40 +32,40 @@ function incorrect(){
   }
 }
 
-//Fisrt Question
+
 let teacherLow = prompt('Have I ever been a teacher?')
 
 let teacher = teacherLow.toLowerCase();
 
 correct();
-//Second Question
+
 let subjectLow = prompt('Do I teach English?')
 
 let subject = subjectLow.toLowerCase();
 
 incorrect();
-//Thrid Question
+
 let coachLow = prompt('Do I coach a sport?')
 
 let coach = coachLow.toLowerCase();
 
 correct();
 
-//Fourth Question
+
 let levelLow = prompt('Do I teach at the Middle School')
 
 let level = levelLow.toLowerCase();
 
 incorrect();
 
-//Fifth Question
+
 let dirationLow = prompt('Have I been teaching for 7 years?')
 
 let diration = dirationLow.toLowerCase();
 
 correct();
 
-//Guess a number question.
+
 let myNumb = 100;
 let number = Math.floor(Math.random() * myNumb + 1);
 let tries = 3;
@@ -96,7 +91,7 @@ while(attempt !== number){
   }
 }
 
-//Guessing from an array. 
+
 alert('Now you are going to get 6 guesses to guess what classes that I teach!')
 const classArray = ['robotics one', 'robotics two', 'robotics three', 'intro to computer science', 'codeing'];
 let classArrayString = classArray.join(', ')
